4be6d1a6d0efcddf63b2e2facf16543ccb8aa833
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2026-04-15 19:12:07 +0530

    Update mustjoin.py

--- bot/mustjoin.py
+++ bot/mustjoin.py
@@ -3,11 +3,11 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 from config import MUST_JOIN_CHANNEL
 
 # Private channel details
-PRIVATE_CHANNEL_ID =  -1003232481172
-PRIVATE_CHANNEL_LINK = "https://t.me/+j-JmRlq0l0U4MTJl"
-PRIVATE_CHANNEL_ID2 =  -1003283874092
+PRIVATE_CHANNEL_ID =  -1003764116093
+PRIVATE_CHANNEL_LINK = "https://t.me/the_destiny_world"
+PRIVATE_CHANNEL_ID2 =  -1003764116093
 
-BOTUSER = "quickcodes_bot"
+BOTUSER = "TgIdStoreProbot"
 # Welcome text with HTML formatting
 WELCOME_TEXT = ("❌ 𝖸𝗈𝗎 𝗆𝗎𝗌𝗍 𝗌𝗎𝖻𝗌𝖼𝗋𝗂𝖻𝖾 𝗍𝗈 𝗍𝗁𝖾 𝗈𝖿𝖿𝗂𝖼𝗂𝖺𝗅 𝖻𝗈𝗍 𝖼𝗁𝖺𝗇𝗇𝖾𝗅 𝗍𝗈 𝗎𝗌𝖾 𝗍𝗁𝖾 𝖻𝗈𝗍...\n\n<blockquote>- <b>𝖢𝗁𝖺𝗇𝗇𝖾𝗅</b> - @vthnet</blockquote>\n\n<b>• Press Verify Button below to check</b>")
 
